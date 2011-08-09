04e73768269919f2de0086761f2d0d965b38d4f8
Emil Karlson <jekarlson@gmail.com>
2011-08-09 08:05:37 +0300

    Cleanup FlushFinish.hs

--- System/OpenCL/Wrappers/FlushFinish.hs
+++ System/OpenCL/Wrappers/FlushFinish.hs
@@ -1,19 +1,15 @@
-{-| Conforms to section 5.10 of the OpenCL 1.0 specification -}
 module System.OpenCL.Wrappers.FlushFinish 
     (clFlush
     ,clFinish)
 where
 
 import System.OpenCL.Wrappers.Types
-import System.OpenCL.Wrappers.Errors
 import System.OpenCL.Wrappers.Utils
 import System.OpenCL.Wrappers.Raw
-import Foreign
-import Control.Applicative
+
 
 clFlush :: CommandQueue -> IO (Maybe ErrorCode)
 clFlush queue = wrapError $ raw_clFlush queue
 
 clFinish :: CommandQueue -> IO (Maybe ErrorCode)
 clFinish queue = wrapError $ raw_clFinish queue
-
