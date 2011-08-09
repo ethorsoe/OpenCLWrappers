351028a9cd7503e79e9298b7a36cca16e169099c
Emil Karlson <jekarlson@gmail.com>
2011-08-09 08:02:43 +0300

    Trivial cleanup of Etc.hs

--- System/OpenCL/Wrappers/Etc.hs
+++ System/OpenCL/Wrappers/Etc.hs
@@ -3,13 +3,10 @@ module System.OpenCL.Wrappers.Etc
     (clGetExtensionFunctionAddress)
 where
 
-import System.OpenCL.Wrappers.Types
-import System.OpenCL.Wrappers.Errors
-import System.OpenCL.Wrappers.Utils
 import System.OpenCL.Wrappers.Raw
-import Foreign
-import Foreign.C
-import Control.Applicative
+import Foreign.Ptr(Ptr)
+import Foreign.C.String(withCString)
+
 
 clGetExtensionFunctionAddress :: String -> IO (Ptr ())
 clGetExtensionFunctionAddress str = withCString str raw_clGetExtensionFunctionAddress
